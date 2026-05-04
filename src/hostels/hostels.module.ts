910654b2c97d004986eab7773d8b58ec1861c8d6
bed-com-05-24 <bed-com-05-24@unima.ac.mw>
2026-05-04 13:24:10 -0700

    dubbuggedd

--- src/hostels/hostels.module.ts
+++ src/hostels/hostels.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { HostelsService } from './hostels.service';
 import { HostelsController } from './hostels.controller';
+import { Hostel } from './entities/hostel.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Hostel])],
   controllers: [HostelsController],
   providers: [HostelsService],
 })
