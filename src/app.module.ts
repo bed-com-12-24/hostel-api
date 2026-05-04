15959bb8b43937c94db63749f763a7fbe58935b0
Bsc-com-06-24 <Bsc-com-06-24@unima.ac.mw>
2026-05-04 10:13:05 +0200

    created notification and payment endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { PaymentModule } from './payment/payment.module';
+import { NotificationModule } from './notification/notification.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, PaymentModule, NotificationModule],
   controllers: [AppController],
   providers: [AppService],
 })
