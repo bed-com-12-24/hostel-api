51fef74101250bb240c3d86651e44aeb07675b2f
Bsc-com-06-24 <Bsc-com-06-24@unima.ac.mw>
2026-05-05 01:53:54 +0200

    silenced hostel folder due to errors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-<<<<<<< HEAD
 import { TypeOrmModule } from '@nestjs/typeorm'; 
-import { AuthModule } from './auth/auth.module';
+//import { AuthModule } from './auth/auth.module';
 import { PaymentModule } from './payment/payment.module';
 import { NotificationModule } from './notification/notification.module';
-import { Auth } from './auth/entities/auth.entity';
+//import { Auth } from './auth/entities/auth.entity';
 import { payment } from './payment/entities/payment.entity';
 import { Notification } from './notification/entities/notification.entity';
 import { AppController } from './app.controller';
@@ -13,8 +12,10 @@ import { AppService } from './app.service';
 import { BookingsModule } from './bookings/bookings.module';
 import { config } from 'process';
 import { booking } from './bookings/entities/booking.entity';
-import { HostelsModule } from './hostels/hostels.module';
-import { Hostel } from './hostels/entities/hostel.entity';
+//import { HostelsModule } from './hostels/hostels.module';
+//import { Hostel } from './hostels/entities/hostel.entity';
+import { ReportsModule } from './reports/reports.module.js';
+import { Report } from './reports/entities/report.entity'
 
 @Module({
   imports:[
@@ -30,43 +31,17 @@ import { Hostel } from './hostels/entities/hostel.entity';
                 password: config.get('DB_PASSWORD'), 
                 serviceName: config.get('DB_SERVICE_NAME'), 
                 synchronize: config.get('DB_SYNCHRONIZE') === 'true', 
-                entities: [Notification, payment, Auth, booking, Hostel], 
+                entities: [Notification, payment, booking, Report], 
                 logging: true,
        }),
      }),
      NotificationModule,
      PaymentModule,
-     AuthModule,
+     //AuthModule,
      BookingsModule,
-     HostelsModule,
+     //HostelsModule,
+     ReportsModule,
     ]
   })
 
-=======
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ReportsModule } from './reports/reports.module.js';
-import { Report } from './reports/entities/report.entity'
- 
- 
-
-
-@Module({
-  imports:[
-  ConfigModule.forRoot({ isGlobal: true }),
-  
-  TypeOrmModule.forRoot({
-  type: 'oracle',
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || '1521'),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  serviceName: process.env.DB_SERVICE_NAME,
-  synchronize: true,
-  entities: [ Report ],
-  logging: true,
-
-}), ReportsModule],
-  
-})
->>>>>>> origin/reports
 export class AppModule {}
