303f6b2cefc4c7d23d5f317aa7d99ff2837163cc
Bsc-com-06-24 <Bsc-com-06-24@unima.ac.mw>
2026-05-05 14:26:02 +0200

    last pushes

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -3,7 +3,7 @@ import { ReportsService } from './reports.service';
 import { CreateReportDto } from './dto/create-report.dto';
 import { UpdateReportDto } from './dto/update-report.dto';
 
-@Controller('api/reports')
+@Controller('reports')
 export class ReportsController {
   constructor(private readonly reportsService: ReportsService) {}
 
